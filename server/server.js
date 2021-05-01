dc7c0ffa5ec3d8e39782e1bb7d2be3e5b0c78bfb
Saif Ullah <mrsaifullah52.1@gmail.com>
2021-05-01 23:25:13 +0500

    Router Has been initialized for Server

--- server/server.js
+++ server/server.js
@@ -2,28 +2,31 @@ const dotenv=require('dotenv');
 const express = require('express');
 const app = express();
 
-
+ 
 dotenv.config( {path: './config.env'} );
 
 // mongodb
 require('./db/conn.js');
 // user schema(Model)
-const User = require('./model/userSchema');
+// const User = require('./model/userSchema');
 // Server Port
 const PORT = process.env.PORT;
 
+// router
+app.use(express.json());
+app.use(require('./router/auth'));
 
 // middlerware
-const middlerware=(req,res,next)=>{
-  console.log("runnning middleware");
-  next();
-}
+// const middlerware=(req,res,next)=>{
+//   console.log("runnning middleware");
+//   next();
+// }
 
 // default homepage
-app.get('/', middlerware, (req,res)=>{
-  res.send("Hello Saif Ullah");
-  console.log("default route");
-});
+// app.get('/', middlerware, (req,res)=>{
+//   res.send("Hello Saif Ullah");
+//   // console.log("default route");
+// });
 
 // listening port
 app.listen(PORT,()=>{
